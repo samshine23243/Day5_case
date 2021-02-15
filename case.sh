a5b35f1c61c93e0dce6da83882e7e87062f45812
samshine <sameershinde23243@gmail.com>
2021-02-15 13:08:30 +0530

    example3 code added

--- case.sh
+++ case.sh
@@ -1,1 +1,28 @@
-echo "case problems"
+echo "Enter Number:"
+read Num
+case $Num in
+   "1")
+      echo "Unit"
+      ;;
+   "10")
+      echo "Ten"
+      ;;
+   "100")
+      echo "Hundred"
+      ;;
+   "1000")
+      echo "Thousand"
+      ;;
+   "10000")
+      echo "Ten Thousand"
+      ;;
+   "100000")
+      echo "Lakh"
+      ;;
+   "1000000")
+      echo "Ten Lakh"
+      ;;
+   *)
+      echo "Wrong Input"
+      ;;
+esac
