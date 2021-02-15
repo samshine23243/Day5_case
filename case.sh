4303c2ddc4284995d57f5fd4f3f81fc769708a48
samshine <sameershinde23243@gmail.com>
2021-02-15 13:06:29 +0530

    example2 code added

--- case.sh
+++ case.sh
@@ -1,1 +1,25 @@
-echo "case problems"
+read -p "Enter nth day in week:" Num
+case $Num in
+   "1")
+      echo "Monday"
+      ;;
+   "2")
+      echo "Tuesday"
+      ;;
+   "3")
+      echo "Wednesday"
+      ;;
+   "4")
+      echo "Thursday"
+      ;;
+   "5")
+      echo "Friday"
+      ;;
+   "6")
+      echo "Saturday"
+      ;;
+   *)
+      echo "Sunday"
+      ;;
+
+esac
